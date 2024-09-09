c248a78bfd3834496527c15121a9cdae4d7914d4
davinci <solomononyango925@gmail.com>
2024-09-09 22:35:40 +0300

    task managemnet restful api

--- Restframework/settings.py
+++ Restframework/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'Items',
+    'Task_Management_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -128,3 +129,14 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': [
+#            'rest_framework.authentication.BasicAuthentication',
+#            'rest_framework.authentication.SessionAuthentication',
+#        ],
+#        'DEFAULT_PERMISSION_CLASSES': [
+#            'rest_framework.permissions.IsAuthenticated',
+#        ],
+#    }
--- Restframework/urls.py
+++ Restframework/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('rest_api.urls')),
-    path('itemapi/',include('Items.urls'))
+    path('itemapi/',include('Items.urls')),
+    path('tasks/',include('Task_Management_app.urls')),
 ]
