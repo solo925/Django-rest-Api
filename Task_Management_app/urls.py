6397f7353d2b1a9546173e7ffb1509e040c36548
davinci <solomononyango925@gmail.com>
2024-09-10 08:55:00 +0300

    rebdering

--- Task_Management_app/urls.py
+++ Task_Management_app/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path,include
 from rest_framework.routers import DynamicRoute,DefaultRouter
-from .views import TaskViewSet,ContentGet
+from .views import TaskViewSet
+from .views import task_list, task_list_db
 
 
 router = DefaultRouter()
-router2 = DynamicRoute('tas',"tasiki","details here",initkwargs="tas")
 router.register(r'task',TaskViewSet,basename='/')
 # route2.register()
 
 
 urlpatterns = [
-    path('',include(router.urls))
+    path('',include(router.urls)),
+        path('tasks/', task_list, name='task_list'),  # Using the API endpoint
+    path('tasks-db/', task_list_db, name='task_list_db'),  # Using the database
 
 ]
--- Task_Management_app/views.py
+++ Task_Management_app/views.py
@@ -2,13 +2,21 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
+import requests
+
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
+# If you want to fetch data directly from the API endpoint
+def task_list(request):
+    response = requests.get('http://localhost:8000/tasks/')  # Replace with your API endpoint
+    tasks = response.json()  # Convert response to JSON format
+    return render(request, 'tasks.html', {'tasks': tasks})
 
-def ContentGet(request):
-    context ={'queryset':queryset}
-    return render(request,'task.html',context)
+# Alternatively, if you want to fetch data directly from the database
+def task_list_db(request):
+    tasks = Task.objects.all()  # Query the database
+    return render(request, 'tasks.html', {'tasks': tasks})
